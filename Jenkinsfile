52fb6198a93e4d3e5da77ed484d74a269495d4de
Atul Kamble <atul_kamble@hotmail.com>
2026-05-25 11:30:18 +0530

    jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,11 +4,13 @@ pipeline {
 
     environment {
 
-        AWS_REGION = "us-east-1"
+        IMAGE_NAME = "cloudnativeapp"
 
-        IMAGE_NAME = "flaskapp"
+        DOCKERHUB_REPO = "bhagyashribari/cloudnativeapp"
 
-        ACCOUNT_ID = credentials('aws-account-id')
+        IMAGE_TAG = "v1"
+
+        DOCKERHUB_USERNAME = "bhagyashribari"
     }
 
     stages {
@@ -17,7 +19,7 @@ pipeline {
 
             steps {
 
-                git 'https://github.com/username/repo.git'
+                git branch: 'dev', url: 'https://github.com/bhagyashri1224/cloudnative-aws-devops-platform.git'
             }
         }
 
@@ -25,7 +27,7 @@ pipeline {
 
             steps {
 
-                sh 'docker build -t flaskapp:v1 .'
+                sh "docker build -t docker.io/${DOCKERHUB_REPO}:${IMAGE_TAG} ."
             }
         }
 
@@ -49,31 +51,32 @@ pipeline {
 
             steps {
 
-                sh 'trivy image flaskapp:v1'
+                sh 'trivy image docker.io/bhagyashribari/cloudnativeapp:v1'
             }
         }
 
-        stage('Push to ECR') {
+        stage('Push to Docker Hub') {
+
+            steps {
+
+                withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKERHUB_USERNAME', passwordVariable: 'DOCKERHUB_PASSWORD')]) {
+                    sh '''
+                    echo "$DOCKERHUB_PASSWORD" | docker login --username "$DOCKERHUB_USERNAME" --password-stdin
+                    docker push docker.io/${DOCKERHUB_REPO}:${IMAGE_TAG}
+                    '''
+                }
+            }
+        }
+
+        stage('Host App on Local Machine') {
 
             steps {
 
                 sh '''
-                aws ecr get-login-password \
-                --region us-east-1 | \
-                docker login \
-                --username AWS \
-                --password-stdin \
-                $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
-                '''
-
-                sh '''
-                docker tag flaskapp:v1 \
-                $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/flaskapp:v1
-                '''
-
-                sh '''
-                docker push \
-                $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/flaskapp:v1
+                docker stop cloudnativeapp || true
+                docker rm cloudnativeapp || true
+                docker run -d -p 5000:5000 --name cloudnativeapp docker.io/${DOCKERHUB_REPO}:${IMAGE_TAG}
+                echo "App hosted on http://localhost:5000"
                 '''
             }
         }
--- terraform/jenkins/main.tf
+++ terraform/jenkins/main.tf
@@ -2,12 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Generate key pair locally and in AWS
+# Generate SSH key locally
 resource "tls_private_key" "jenkins" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+# Save private key locally
 resource "local_file" "private_key" {
   content              = tls_private_key.jenkins.private_key_pem
   filename             = "${path.module}/jenkins.pem"
@@ -15,31 +16,35 @@ resource "local_file" "private_key" {
   directory_permission = "0700"
 }
 
+# Upload public key to AWS
 resource "aws_key_pair" "jenkins_key" {
-  key_name   = "jenkins"
+  key_name   = "jenkins-key-new"
   public_key = tls_private_key.jenkins.public_key_openssh
 }
 
-# Security group to allow SSH, HTTP, HTTPS, and Jenkins port
+# Security Group
 resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins-sg"
-  description = "Allow SSH, HTTP, HTTPS, and Jenkins"
+  name        = "jenkins-new-sg"
+  description = "Allow SSH, Jenkins, HTTP, HTTPS"
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-    ingress {
-    from_port   = 9000
-    to_port     = 9000
+  ingress {
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -47,6 +52,7 @@ resource "aws_security_group" "jenkins_sg" {
   }
 
   ingress {
+    description = "HTTPS"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -54,8 +60,9 @@ resource "aws_security_group" "jenkins_sg" {
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    description = "SonarQube"
+    from_port   = 9000
+    to_port     = 9000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -66,12 +73,16 @@ resource "aws_security_group" "jenkins_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "jenkins-sg"
+  }
 }
 
-# Launch EC2 instance and provision via SSH
+# EC2 Instance
 resource "aws_instance" "jenkins" {
   ami                    = "ami-0bdd88bd06d16ba03" # Amazon Linux 2023
-  instance_type          = "t3.large"
+  instance_type          = "c7i-flex.large"
   key_name               = aws_key_pair.jenkins_key.key_name
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
 
@@ -85,33 +96,56 @@ resource "aws_instance" "jenkins" {
     Name = "Jenkins"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo yum install -y git docker maven tree",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
-      "sudo dnf install -y java-21-amazon-corretto-devel",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
-      "sudo dnf install -y jenkins",
-      "sudo usermod -aG docker jenkins",
-      "sudo usermod -aG docker ec2-user",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl enable jenkins",
-      "sudo systemctl start jenkins",
-      "echo 'Waiting for Jenkins to initialize...'",
-      "for i in $(seq 1 24); do if sudo test -f /var/lib/jenkins/secrets/initialAdminPassword; then break; fi; echo \"Attempt $i/24 - waiting...\"; sleep 5; done",
-      "echo 'Jenkins Admin Password:'",
-      "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
-    ]
-  }
-
-
   connection {
     type        = "ssh"
     user        = "ec2-user"
     private_key = tls_private_key.jenkins.private_key_pem
     host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+
+      # Wait for cloud-init
+      "while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 5; done",
+
+      # System update
+      "sudo dnf update -y",
+
+      # Install packages
+      "sudo dnf install -y git docker maven tree wget java-21-amazon-corretto-devel",
+
+      # Enable Docker
+      "sudo systemctl enable docker",
+      "sudo systemctl start docker",
+
+      # Docker permissions
+      "sudo usermod -aG docker ec2-user",
+
+      # Add Jenkins repo
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+
+      # Import Jenkins key
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
+
+      # Install Jenkins
+      "sudo dnf install -y jenkins",
+
+      # Add Jenkins to Docker group
+      "sudo usermod -aG docker jenkins",
+
+      # Enable Jenkins
+      "sudo systemctl daemon-reload",
+      "sudo systemctl enable jenkins",
+      "sudo systemctl start jenkins",
+
+      # Wait for Jenkins password
+      "echo 'Waiting for Jenkins to initialize...'",
+      "for i in $(seq 1 30); do if sudo test -f /var/lib/jenkins/secrets/initialAdminPassword; then break; fi; echo Waiting... $i; sleep 10; done",
+
+      # Print password
+      "echo 'Jenkins Initial Admin Password:'",
+      "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
+    ]
   }
 }
