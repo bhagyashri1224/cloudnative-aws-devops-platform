f938fd297f738f9ab1cdd03c7f7948e2f28fe8c4
Atul Kamble <atul_kamble@hotmail.com>
2026-05-28 13:30:31 +0530

    changes

--- app/test_app.py
+++ app/test_app.py
@@ -1,9 +1,11 @@
 from app import app
 
-def test_home():
 
+def test_home():
+    app.config['TESTING'] = True
     client = app.test_client()
 
     response = client.get('/')
 
     assert response.status_code == 200
+    assert b'hostname' in response.data or b'Hello' in response.data
